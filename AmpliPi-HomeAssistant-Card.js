02a89e8e06eee445c47e64052b1ccf4b478863b7
Steven Engelbert <steve@micro-nova.com>
2025-05-08 17:01:01 -0400

    See if I can print individual portions of source player in lieu of using debug prints

--- AmpliPi-HomeAssistant-Card.js
+++ AmpliPi-HomeAssistant-Card.js
@@ -1081,6 +1081,7 @@
               ${this._media_player == undefined ? "" : this._media_player}
               <br><b>Source:</b>
               ${this._source_player == undefined ? "" : this._source_player}
+              ${this._source_player == undefined ? "" : this._source_player.source}
               <b>Stream:</b>
               ${this._stream_player == undefined ? "" : this._stream_player}
               <hr>
