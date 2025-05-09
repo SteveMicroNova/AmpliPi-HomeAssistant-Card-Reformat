4de301fe4f6cd151768f86ab57775c95e27d59f4
Steven Engelbert <steve@micro-nova.com>
2025-05-09 13:35:41 -0400

    change how entity id is read

--- AmpliPi-HomeAssistant-Card.js
+++ AmpliPi-HomeAssistant-Card.js
@@ -851,7 +851,7 @@
           var source_id;
           if (!is_source) {
               const source_num = source.split(" ")[1] - 1;
-              for (var [name, entity] of Object.entries(this._hass.states))if (entity.attributes.amplipi_source_id !== undefined && entity.attributes.amplipi_source_id === source_num) source_id = entity.id;
+              for (var [name, entity] of Object.entries(this._hass.states))if (entity.attributes.amplipi_source_id !== undefined && entity.attributes.amplipi_source_id === source_num) source_id = entity.attributes.id;
           } else source_id = source;
           let source_player_config1 = {
               "type": "custom:mini-media-player",
